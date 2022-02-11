658f04b1d82f3eae904a13d6a3fc0bbaa2f4d932
Guilherme Alcântara <gui.absilva@outlook.com>
2022-02-11 00:27:20 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    background: black;
+    color: #29292e;
+  }
+  
+  body,
+  input,
+  button,
+  textarea {
+      font: 400 16px 'Roboto', sans-serif;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,155 @@
+<template>
+  <div id="card-page">
+    <aside>
+      <ColorPicker
+        style="width: 218px"
+        theme="dark"
+        :color="color01"
+        :sucker-hide="true"
+        @changeColor="changeColor"
+      />
+      <ColorPicker
+        style="width: 218px"
+        theme="dark"
+        :color="color02"
+        :sucker-hide="true"
+        @changeColor="changeColor02"
+      />
+      <input v-model="name" type="text" name="" />
+    </aside>
+    <main>
+      <div class="card-container">
+        <div class="card">
+          <div class="card-front" :style="cardColor">
+            <img class="positioning" src="../assets/logo.png" alt="" />
+            <p class="positioning">{{ name }}</p>
+          </div>
+          <div class="card-back" :style="cardColorBack">
+            <p class="positioning">aaaaaaaaaaaaaaaaa</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { ColorPicker } from "vue-color-kit";
+import "vue-color-kit/dist/vue-color-kit.css";
+export default {
+  components: {
+    ColorPicker,
+  },
+  data() {
+    return {
+      name: "",
+      color01: "#000000",
+      color02: "#414141",
+    };
+  },
+  computed: {
+    cardColor() {
+      return {
+        backgroundImage: `linear-gradient(315deg, ${this.color01} 0%, ${this.color02} 74%)`,
+      };
+    },
+    cardColorBack() {
+      return {
+        backgroundImage: `linear-gradient(52deg, ${this.color01} 0%, ${this.color02} 74%)`,
+      };
+    },
+  },
+  methods: {
+    changeColor(color01) {
+      const { r, g, b, a } = color01.rgba;
+      this.color01 = `rgba(${r}, ${g}, ${b}, ${a})`;
+    },
+    changeColor02(color02) {
+      const { r, g, b, a } = color02.rgba;
+      this.color02 = `rgba(${r}, ${g}, ${b}, ${a})`;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#card-page {
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
+
+  aside {
+    display: flex;
+    flex: 1;
+    padding: 0 32px;
+    background: red;
+    align-items: center;
+    justify-content: center;
+  }
+
+  main {
+    display: flex;
+    flex: 1;
+    padding: 0 32px;
+    align-items: center;
+    justify-content: center;
+
+    .card-container {
+      position: relative;
+      background-color: transparent;
+      width: 400px;
+      height: 300px;
+      perspective: 1000px;
+
+      .card {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: transform ease 1.6s;
+        transform-style: preserve-3d;
+      }
+
+      &:hover .card {
+        transform: rotateY(180deg) rotate(-30deg) skew(25deg, 6deg) scale(1.05)
+          skewY(-20deg);
+      }
+
+      .card-front {
+        transform: rotate(20deg) skew(-15deg, 3deg);
+
+        img {
+          position: absolute;
+          width: 60px;
+          top: 20px;
+          left: 320px;
+        }
+
+        p {
+          width: 400px;
+          position: absolute;
+          color: white;
+          top: 80%;
+          left: -100px;
+          font-weight: bold;
+        }
+        
+      }
+      .card-back {
+        transform: rotateY(180deg) rotate(-25deg) skew(15deg, 0deg);
+      }
+
+      .card-front,
+      .card-back {
+        width: 400px;
+        height: 240px;
+        border-radius: 15px;
+        position: absolute;
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        box-shadow: 13px 13px 13px 0px rgba(0, 0, 0, 0.75);
+      }
+    }
+  }
+}
+</style>
