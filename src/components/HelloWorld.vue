eea069c6c39215f70994acb5a2e7e1144d9d7816
Guilherme Alcântara <gui.absilva@outlook.com>
2022-02-11 15:26:48 -0300

    Adição da animação e Nav

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,39 @@
 <template>
   <div id="card-page">
-    <aside>
-      <ColorPicker
-        style="width: 218px"
-        theme="dark"
-        :color="color01"
-        :sucker-hide="true"
-        @changeColor="changeColor"
-      />
-      <ColorPicker
-        style="width: 218px"
-        theme="dark"
-        :color="color02"
-        :sucker-hide="true"
-        @changeColor="changeColor02"
-      />
-      <input v-model="name" type="text" name="" />
-    </aside>
+    <nav>
+      <h2>Guilherme Alcantara</h2>
+    </nav>
     <main>
-      <div class="card-container">
-        <div class="card">
-          <div class="card-front" :style="cardColor">
-            <img class="positioning" src="../assets/logo.png" alt="" />
-            <p class="positioning">{{ name }}</p>
-          </div>
-          <div class="card-back" :style="cardColorBack">
-            <p class="positioning">aaaaaaaaaaaaaaaaa</p>
+      <aside>
+        <ColorPicker
+          style="width: 218px"
+          theme="dark"
+          :color="color01"
+          :sucker-hide="true"
+          @changeColor="changeColor"
+        />
+        <ColorPicker
+          style="width: 218px"
+          theme="dark"
+          :color="color02"
+          :sucker-hide="true"
+          @changeColor="changeColor02"
+        />
+        <input v-model="name" type="text" name="" />
+      </aside>
+      <section>
+        <div class="card-container">
+          <div class="card">
+            <div class="card-front" :style="cardColor">
+              <img class="positioning" src="../assets/logo.png" alt="" />
+              <p class="positioning">{{ name }}</p>
+            </div>
+            <div class="card-back" :style="cardColorBack">
+              <p class="positioning">aaaaaaaaaaaaaaaaa</p>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </main>
   </div>
 </template>
@@ -75,82 +80,110 @@ export default {
 <style scoped lang="scss">
 #card-page {
   display: flex;
+  flex-direction: column;
   height: 100vh;
   overflow: hidden;
   background-image: url("../assets/bg.png");
   background-size: cover;
   background-repeat: no-repeat;
-  aside {
-    display: flex;
-    flex: 1;
-    padding: 0 32px;
-    align-items: center;
-    justify-content: center;
-  }
 
+  nav {
+    width: 100%;
+    height: 80px;
+    text-align: center;
+    color: white;
+    padding: 10px 32px;
+  }
   main {
     display: flex;
-    flex: 1;
-    padding: 0 32px;
-    align-items: center;
-    justify-content: center;
-    .card-container {
-      position: relative;
-      background-color: transparent;
-      width: 400px;
-      height: 300px;
-      perspective: 1000px;
+    height: 100%;
 
-      .card {
+    aside {
+      display: flex;
+      flex: 1;
+      padding: 0 32px;
+      align-items: center;
+      justify-content: center;
+    }
+
+    section {
+      display: flex;
+      flex: 1;
+      padding: 0 32px;
+      align-items: center;
+      justify-content: center;
+
+      .card-container {
         position: relative;
-        width: 100%;
-        height: 100%;
-        text-align: center;
-        transition: transform ease 1.6s;
-        transform-style: preserve-3d;
-      }
-
-      &:hover .card {
-        transform: rotateY(180deg) rotate(-30deg) skew(25deg, 6deg) scale(1.05)
-          skewY(-20deg);
-      }
-
-      .card-front {
-        transform: rotate(20deg) skew(-15deg, 3deg);
-
-        img {
-          position: absolute;
-          width: 60px;
-          top: 20px;
-          left: 320px;
-        }
-
-        p {
-          min-width: 360px;
-          max-width: 360px;
-          text-align: left;
-          position: absolute;
-          color: white;
-          top: 80%;
-          left: 25px;
-          font-weight: bold;
-        }
-      }
-      .card-back {
-        transform: rotateY(180deg) rotate(-25deg) skew(15deg, 0deg);
-      }
-
-      .card-front,
-      .card-back {
+        background-color: transparent;
         width: 400px;
-        height: 240px;
-        border-radius: 15px;
-        position: absolute;
-        -webkit-backface-visibility: hidden;
-        backface-visibility: hidden;
-        box-shadow: 13px 13px 13px 0px rgba(0, 0, 0, 0.75);
+        height: 300px;
+        perspective: 1000px;
+        animation: bounce ease-in-out 2.5s infinite;
+
+        .card {
+          position: relative;
+          width: 100%;
+          height: 100%;
+          text-align: center;
+          transition: transform ease 1.6s;
+          transform-style: preserve-3d;
+        }
+
+        &:hover .card {
+          transform: rotateY(180deg) rotate(-30deg) skew(25deg, 6deg)
+            scale(1.05) skewY(-20deg);
+        }
+
+        .card-front {
+          transform: rotate(20deg) skew(-15deg, 3deg);
+
+          img {
+            position: absolute;
+            width: 60px;
+            top: 20px;
+            left: 320px;
+          }
+
+          p {
+            min-width: 360px;
+            max-width: 3px;
+            text-align: left;
+            position: absolute;
+            color: white;
+            top: 80%;
+            left: 25px;
+            font-weight: bold;
+          }
+        }
+
+        .card-back {
+          transform: rotateY(180deg) rotate(-25deg) skew(15deg, 0deg);
+        }
+
+        .card-front,
+        .card-back {
+          width: 400px;
+          height: 240px;
+          border-radius: 15px;
+          position: absolute;
+          -webkit-backface-visibility: hidden;
+          backface-visibility: hidden;
+          box-shadow: 13px 13px 13px 0px rgba(0, 0, 0, 0.75);
+        }
       }
     }
+  }
+}
+@keyframes bounce {
+  0% {
+    transform: translateY(0%);
+  }
+  50% {
+    transform: translateY(5%);
+  }
+  100% {
+    transform: translateY(0%);
   }
 }
 </style>
